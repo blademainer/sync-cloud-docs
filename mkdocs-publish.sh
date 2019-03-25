6a0f8f2ee4aee9b46770b72e5eb07bfd9b84624c
blademainer <blademainer@gmail.com>
2019-03-26 00:19:01 +0800

    fixed build

--- mkdocs-publish.sh
+++ mkdocs-publish.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v ~:/root -v `pwd`:/docs -w /docs pjoc/mkdocs:v0.0.10 mkdocs gh-deploy
+docker run --rm -v ~:/root -v `pwd`:/docs -w /docs pjoc/mkdocs:v0.0.10 mkdocs gh-deploy -v

c1ad81524829a092ccc4bfc2682ac10c0cbb6619
blademainer <blademainer@gmail.com>
2019-03-25 23:57:25 +0800

    update git

--- mkdocs-publish.sh
+++ mkdocs-publish.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v ~/.ssh:/root/.ssh -v `pwd`:/docs -w /docs pjoc/mkdocs:v0.0.10 mkdocs gh-deploy
+docker run --rm -v ~:/root -v `pwd`:/docs -w /docs pjoc/mkdocs:v0.0.10 mkdocs gh-deploy
